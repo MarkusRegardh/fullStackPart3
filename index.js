75fd7ebe216efac03457a731b448ef3ec3ace881
markus <regardm1@aalto.fi>
2023-03-16 18:03:32 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(function (tokens,req,res) {
   const result =  [
     tokens.method(req, res),
